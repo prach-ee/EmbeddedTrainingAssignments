a4027d9d196e33c97cf3b29229760a2a5371f88a
prach-ee <85980479+prach-ee@users.noreply.github.com>
2021-06-16 18:55:04 +0530

    Create Q1.c

---
+++ Assignment-1/Q1.c
@@ -0,0 +1,9 @@
+//program for nested comments
+
+#include<stdio.h>
+int main(){
+	/*printf("Hello");
+	*this line won't give error as it is commented out*/
+	*but this line will give error as multiline comment ends with second"*/"
+	*/
+}
