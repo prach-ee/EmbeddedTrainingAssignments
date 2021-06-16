66c71a2b6ca47ca4c1ab7e6ae59c1540dba045a2
prach-ee <85980479+prach-ee@users.noreply.github.com>
2021-06-16 19:13:22 +0530

    Create Q3.c

---
+++ Assignment-1/Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+char ch1=65,ch2='b',ch3='3';
+
+printf("ch1 = %c\n",ch1);//output ch1 = A
+printf("ch2 = %c\n",ch2);//output ch2 = b
+printf("ch3 = %c\n",ch3);//output ch3 = 3
+printf("ch1 = %d\n",ch1);//output ch1 = 65
+printf("ch2 = %d\n",ch2);//output ch2 = 98
+printf("ch3 = %d\n",ch3);//output ch3 = 51
+
+}
